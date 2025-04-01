a488dbe3df1cdf6f23bf14f049c677f924ed488d
Damillare Ajayi <darea649@gmail.com>
2025-04-01 07:18:46 +0100

    resolved cors

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,24 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Ensure CORS matches frontend
+  // app.enableCors({
+  //   origin: [
+  //     'http://localhost:3000', // Dev environment
+  //     'https://stonepays-app.netlify.app' , // Production frontend
+  //   ],
+  //   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  //   allowedHeaders: ['Content-Type', 'Authorization'],
+  //   credentials: true, // Allow cookies and authentication headers
+  // });
+
+
   app.enableCors({
-    origin: [
-      'http://localhost:3000', // Dev environment
-      'https://stonepays-app.netlify.app' , // Production frontend
-    ],
+    origin: true, // Allows all origins
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true, // Allow cookies and authentication headers
   });
+  
 
   // Swagger Documentation
   const config = new DocumentBuilder()
